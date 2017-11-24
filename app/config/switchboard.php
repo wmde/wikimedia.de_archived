f7d9630f7019e0671505c2d44f576197d812f498
David Persson <nperson@gmx.de>
2017-11-24 15:50:23 +0100

    Re-license under AGPL v3.

--- app/config/switchboard.php
+++ app/config/switchboard.php
@@ -3,7 +3,7 @@
  * Copyright 2013 David Persson. All rights reserved.
  * Copyright 2016 Atelier Disko. All rights reserved.
  *
- * Use of this source code is governed by a BSD-style
+ * Use of this source code is governed by the AGPL v3
  * license that can be found in the LICENSE file.
  */
 
