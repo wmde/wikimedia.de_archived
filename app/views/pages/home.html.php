ec57761b9595940beea81efb2664d0ff7f2b96e4
David Persson <nperson@gmx.de>
2017-11-28 16:12:42 +0100

    Cleanup newsSlider dummy HTML.

--- app/views/pages/home.html.php
+++ app/views/pages/home.html.php
@@ -51,21 +51,23 @@ $this->title($site->title());
 			Ipsum
 		</a>
 	</section>
+
 	<section class="news news-slider-wrapper sc--lightgray">
 		<div class="limit--14 center-column cp--h1 cp--v2">
 			<h1 class="tl--beta t--strong h--alpha">Aktuelles</h1>
+
 			<div class="news-slider">
 				<div class="news-slider__counter tm--gamma t--strong">
 					?/?
 				</div>
+
 				<a href="#next-slide" class="news-slider__next"></a>
+
 				<div class="news-slider__stage">
 					<article class="news-slider__slide active is-wm-type">
-
 						<div class="news-slider__cover limit--5">
 							<?= $this->assets->image('/app/img/dummy.jpg') ?>
 						</div>
-
 						<div class="news-slider__content limit--8 cp--0-5">
 							<h1 class="news-slider__type tm--alpha t--caps t--strong">Projekte</h1>
 
@@ -87,37 +89,53 @@ $this->title($site->title());
 							</div>
 						</div>
 					</article>
-					<?php for ($i = 0; $i < 4; $i++): ?>
-					<?php
-	$class = ['next is-wm-type', 'is-member-type', 'is-iniative-type', 'is-member-type'][$i];
-?>
-					<article class="news-slider__slide <?= $class ?>">
 
+					<article class="news-slider__slide next is-wm-type">
 						<div class="news-slider__cover limit--5">
 							<?= $this->assets->image('/app/img/dummy.jpg') ?>
 						</div>
-
 						<div class="news-slider__content limit--8 cp--0-5">
 							<h1 class="news-slider__type tm--alpha t--caps t--strong">Projekte</h1>
 
-							<p class="news-slider__teaser tm--alpha t--strong">
+							<p class="tm--alpha t--strong">
 								<!--lorem-->Ultrices posuere cubilia Curae; Vestibulum hendrerit malesuada odio. Fusce ut elit ut augue sollicitudin blandit. Phasellus volutpat lorem. Duis non pede.<!--/lorem-->
 							</p>
-							<p class="news-slider__text tm--beta">
+							<p class="tm--beta">
 								<!--lorem-->Eu, dui. Quisque dignissim consequat nisl. Pellentesque porta augue in diam. Duis mattis. Aliquam et mi quis turpis pellentesque consequat. Suspendisse.<!--/lorem-->
 							</p>
-							<div class="news-slider__actions">
-								<?= $this->html->link('Zum Projekt', '#', [
-									'class' => 'news-slider__action link--black ts--alpha t--strong'
+							<div>
+								<?= $this->html->link('Zu diesem Projekt', '#', [
+									'class' => 'link--black ts--alpha t--strong'
 								]) ?>
 							</div>
 						</div>
 					</article>
-					<?php endfor ?>
+
+					<article class="news-slider__slide is-wm-type">
+						<div class="news-slider__cover limit--5">
+							<?= $this->assets->image('/app/img/dummy.jpg') ?>
+						</div>
+						<div class="news-slider__content limit--8 cp--0-5">
+							<h1 class="news-slider__type tm--alpha t--caps t--strong">Projekte</h1>
+
+							<p class="tm--alpha t--strong">
+								<!--lorem-->Ultrices posuere cubilia Curae; Vestibulum hendrerit malesuada odio. Fusce ut elit ut augue sollicitudin blandit. Phasellus volutpat lorem. Duis non pede.<!--/lorem-->
+							</p>
+							<p class="tm--beta">
+								<!--lorem-->Eu, dui. Quisque dignissim consequat nisl. Pellentesque porta augue in diam. Duis mattis. Aliquam et mi quis turpis pellentesque consequat. Suspendisse.<!--/lorem-->
+							</p>
+							<div>
+								<?= $this->html->link('Zu diesem Projekt', '#', [
+									'class' => 'link--black ts--alpha t--strong'
+								]) ?>
+							</div>
+						</div>
+					</article>
 				</div>
 			</div>
 		</div>
 	</section>
+
 	<section class="team limit--14 cp--t1-25 cp--b4 center-column sc--white">
 		<h1 class="tl--beta t--strong h--alpha">Unser Präsidium</h1>
 		<div class="team__inner">
