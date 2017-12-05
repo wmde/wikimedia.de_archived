a25187f88c3eecdbc6f1c8adf35dd9716cd11dee
David Persson <nperson@gmx.de>
2017-12-05 12:33:55 +0100

    Add cms_post; (update deps).

--- data/schema.sql
+++ data/schema.sql
@@ -1,30 +1,4 @@
-# ************************************************************
-# Sequel Pro SQL dump
-# Version 4541
-#
-# http://www.sequelpro.com/
-# https://github.com/sequelpro/sequelpro
-#
-# Host: 127.0.0.1 (MySQL 5.5.5-10.0.32-MariaDB-0+deb8u1)
-# Datenbank: wmde_site
-# Erstellt am: 2017-11-24 13:35:05 +0000
-# ************************************************************
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
-
-# Export von Tabelle media
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `media`;
-
+-- Create syntax for TABLE 'media'
 CREATE TABLE `media` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `owner_id` int(11) unsigned NOT NULL,
@@ -39,13 +13,7 @@ CREATE TABLE `media` (
   KEY `user` (`owner_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Unique index set on user_id+checksum instead of user_id+dirn';
 
-
-
-# Export von Tabelle media_attachments
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `media_attachments`;
-
+-- Create syntax for TABLE 'media_attachments'
 CREATE TABLE `media_attachments` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `media_id` int(11) unsigned NOT NULL,
@@ -56,13 +24,7 @@ CREATE TABLE `media_attachments` (
   KEY `media_file` (`media_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
-
-# Export von Tabelle media_versions
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `media_versions`;
-
+-- Create syntax for TABLE 'media_versions'
 CREATE TABLE `media_versions` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `media_id` int(11) unsigned NOT NULL,
@@ -79,13 +41,42 @@ CREATE TABLE `media_versions` (
   KEY `media_id` (`media_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Unique index set on user_id+checksum instead of user_id+dirn';
 
+-- Create syntax for TABLE 'posts'
+CREATE TABLE `posts` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `owner_id` int(11) unsigned NOT NULL,
+  `site` varchar(50) DEFAULT NULL,
+  `cover_media_id` int(11) DEFAULT NULL,
+  `authors` varchar(250) DEFAULT NULL,
+  `title` varchar(250) DEFAULT '',
+  `teaser` text,
+  `body` text,
+  `tags` varchar(250) DEFAULT NULL,
+  `source` varchar(250) DEFAULT NULL,
+  `is_promoted` tinyint(1) DEFAULT '0',
+  `is_published` tinyint(1) DEFAULT '0',
+  `published` date NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `is_published` (`is_published`),
+  KEY `is_promoted` (`is_promoted`),
+  KEY `published` (`published`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- Create syntax for TABLE 'tags'
+CREATE TABLE `tags` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(250) NOT NULL DEFAULT '',
+  `title` varchar(250) DEFAULT '',
+  `description` text,
+  `is_published` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-# Export von Tabelle team_members
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `team_members`;
-
+-- Create syntax for TABLE 'team_members'
 CREATE TABLE `team_members` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `owner_id` int(11) unsigned NOT NULL,
@@ -103,15 +94,9 @@ CREATE TABLE `team_members` (
   `created` datetime NOT NULL,
   `modified` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-
-
-# Export von Tabelle users
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `users`;
-
+-- Create syntax for TABLE 'users'
 CREATE TABLE `users` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `uuid` varchar(36) NOT NULL DEFAULT '',
@@ -137,13 +122,3 @@ CREATE TABLE `users` (
   UNIQUE KEY `number` (`number`),
   KEY `session_key` (`session_key`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-
-
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
