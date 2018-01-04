c144e3cc99bb91eee68fac44deed76f12da00ac5
Phillip Klein <phillip@atelierdisko.de>
2018-01-04 14:31:14 +0100

    Add google analytics info to imprint.

--- app/views/pages/imprint.html.php
+++ app/views/pages/imprint.html.php
@@ -67,6 +67,62 @@ $contact = Settings::read('contact');
 						Schaden an, nehmen minimalen Speicherplatz in Anspruch und
 						enthalten keine Viren.
 					</li>
+					<li class="tm--beta">
+						Google Analytics: Diese Website benutzt Google Analytics, einen
+						Webanalysedienst der Google Inc. („Google“). Google Analytics
+						verwendet sog. „Cookies“, Textdateien, die auf Ihrem Computer
+						gespeichert werden und die eine Analyse der Benutzung der Website
+						durch Sie ermöglichen. Die durch das Cookie erzeugten
+						Informationen über Ihre Benutzung dieser Website werden in der
+						Regel an einen Server von Google in den USA übertragen und dort
+						gespeichert. Im Falle der Aktivierung der IP-Anonymisierung auf
+						dieser Website, wird Ihre IP-Adresse von Google jedoch innerhalb
+						von Mitgliedstaaten der Europäischen Union oder in anderen
+						Vertragsstaaten des Abkommens über den Europäischen
+						Wirtschaftsraum zuvor gekürzt. Nur in Ausnahmefällen wird die
+						volle IP-Adresse an einen Server von Google in den USA
+						übertragen und dort gekürzt. Im Auftrag des Betreibers dieser
+						Website wird Google diese Informationen benutzen, um Ihre
+						Nutzung der Website auszuwerten, um Reports über die
+						Websiteaktivitäten zusammenzustellen und um weitere mit der
+						Websitenutzung und der Internetnutzung verbundene
+						Dienstleistungen gegenüber dem Websitebetreiber zu erbringen. Die
+						im Rahmen von Google Analytics von Ihrem Browser übermittelte
+						IP-Adresse wird nicht mit anderen Daten von Google
+						zusammengeführt. Sie können die Speicherung der Cookies durch
+						eine entsprechende Einstellung Ihrer Browser-Software verhindern;
+						wir weisen Sie jedoch darauf hin, dass Sie in diesem Fall
+						gegebenenfalls nicht sämtliche Funktionen dieser Website
+						vollumfänglich werden nutzen können. Sie können darüber hinaus
+						die Erfassung der durch das Cookie erzeugten und auf Ihre Nutzung
+						der Website bezogenen Daten (inkl. Ihrer IP-Adresse) an Google
+						sowie die Verarbeitung dieser Daten durch Google verhindern,
+						indem Sie das unter dem folgenden Link (<a
+							href="http://tools.google.com/dlpage/gaoptout?hl=de"
+							target="new"
+							class="link--black"
+						>http://tools.google.com/dlpage/gaoptout?hl=de</a>) verfügbare
+						Browser-Plugin herunterladen und installieren.
+						Sie können die Erfassung durch Google Analytics verhindern, indem
+						Sie auf folgenden Link klicken. Es wird ein Opt-Out-Cookie
+						gesetzt, das die zukünftige Erfassung Ihrer Daten beim Besuch
+						dieser Website verhindert: <a
+							href=“javascript:gaOptout()“
+							class="link--black"
+						>Google Analytics deaktivieren</a> Nähere Informationen zu
+						Nutzungsbedingungen und Datenschutz finden Sie unter <a
+							href="http://www.google.com/analytics/terms/de.html"
+							target="new"
+							class="link--black"
+						>http://www.google.com/analytics/terms/de.html</a> bzw. unter <a
+							href="https://www.google.de/intl/de/policies/"
+							target="new"
+							class="link--black"
+						>https://www.google.de/intl/de/policies/</a>. Wir weisen Sie
+						darauf hin, dass auf dieser Website Google Analytics um den Code
+						„anonymizeIp“ erweitert wurde, um eine anonymisierte Erfassung
+						von IP-Adressen (sog. IP-Masking) zu gewährleisten.
+					</li>
 				</ul>
 			</p>
 		</section>
