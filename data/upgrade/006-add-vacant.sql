26e5ae94034d7173d0afad45b71530b1f5c0bf66
Marius Wilms <mariuswilms@gmx.de>
2018-04-10 17:13:19 +0200

    Add missing migrations and upgrade bundle

---
+++ data/upgrade/006-add-vacant.sql
@@ -0,0 +1,14 @@
+CREATE TABLE `vacant_team_positions` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `owner_id` int(11) unsigned NOT NULL,
+  `order` int(11) unsigned DEFAULT NULL,
+  `title` varchar(250) DEFAULT NULL,
+  `department` varchar(250) DEFAULT NULL,
+  `locations` varchar(250) DEFAULT NULL,
+  `teaser` text,
+  `body` text,
+  `is_published` tinyint(1) DEFAULT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
---
+++ data/upgrade/007-rename-portrait.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `team_members` CHANGE `portrait_media_id` `cover_media_id` INT(11)  UNSIGNED  NULL  DEFAULT NULL;
