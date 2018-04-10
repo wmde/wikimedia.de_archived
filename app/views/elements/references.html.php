28db867d569b972cde71be67cc9b8ebe45f1ba97
Marius Wilms <mariuswilms@gmx.de>
2018-04-10 18:01:35 +0200

    Cleanup references element

--- app/views/elements/references.html.php
+++ app/views/elements/references.html.php
@@ -1,14 +1,6 @@
-<?php
-
-extract([
-	// A collection of Reference entities.
-	'data' => []
-], EXTR_SKIP);
-
-?>
 <div class="refs-foldout ts--beta">
 	<div class="limit--20 center-column cp--h1 cp--v1">
-	<a class="refs-foldout__close" href="#close"><?= $t("Referenzliste schließen") ?></a>
+	<a class="refs-foldout__close" href="#close"><?= $t('Referenzliste schließen') ?></a>
 
 		<div class="refs-foldout__content">
 			<?= $this->references->index() ?>
