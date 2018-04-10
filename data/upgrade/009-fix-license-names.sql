2548d00c8023cefe5bbf09c12493dafceb2b52e2
Marius Wilms <mariuswilms@gmx.de>
2018-04-10 18:02:20 +0200

    Fix license names

---
+++ data/upgrade/009-fix-license-names.sql
@@ -0,0 +1,3 @@
+UPDATE `references` SET `license` = 'CC-BY-SA-3.0' WHERE `id` = '19';
+UPDATE `references` SET `license` = 'CC-BY-SA-3.0' WHERE `id` = '21';
+UPDATE `references` SET `license` = 'CC-BY-2.0' WHERE `id` = '22';
